9cc339c1a65848564b09edc17471d13e9bc7ab18
Beherith <mysterme@gmail.com>
2019-05-30 17:32:08 +0200

    Update tinkercad_brick_wall_generator.js

--- tinkercad_brick_wall_generator.js
+++ tinkercad_brick_wall_generator.js
@@ -1,4 +1,4 @@
-// Convenience Declarations For Dependencies.
+/ Convenience Declarations For Dependencies.
 // 'Core' Is Configured In Libraries Section.
 // Some of these may not be used by this example.
 var Conversions = Core.Conversions;
@@ -41,9 +41,9 @@ var Vector3D = Core.Vector3D;
 		}
 
 		function seededrandom3Darr(forpoint,multiplier, scaler){
-			var pt = [Math.sin( (forpoint.x + forpoint.y + forpoint.z)*multiplier*1.1)*scaler,
-					Math.sin( (forpoint.x + forpoint.y + forpoint.z)*multiplier*1.1)*scaler,
-					Math.sin( (forpoint.x + forpoint.y + forpoint.z)*multiplier*1.1)*scaler];
+			var pt = [Math.sin( (forpoint[0] + forpoint[1] + forpoint[2])*multiplier*1.1)*scaler,
+					  Math.sin( (forpoint[0] + forpoint[1] + forpoint[2])*multiplier*1.1)*scaler,
+					  Math.sin( (forpoint[0] + forpoint[1] + forpoint[2])*multiplier*1.1)*scaler];
 			return pt;
 		}
       
@@ -51,42 +51,51 @@ var Vector3D = Core.Vector3D;
 			return new Point3D(Math.sin( (forpoint.x + forpoint.y + forpoint.z)*multiplier*1.1)*scaler,
 					Math.sin( (forpoint.x + forpoint.y + forpoint.z)*multiplier*1.1)*scaler,
 					Math.sin( (forpoint.x + forpoint.y + forpoint.z)*multiplier*1.1)*scaler);
-	
 		}
 		
 		function pt2a(point){
 			var retp = [point.x,point.y,point.z];
 			return retp;
 		}
+		
+		function addmul(a,b,m){
+			return [a[0]+m*b[0],a[1]+m*b[1],a[2]+m*b[2]];
+		}
+		
 		function addbrick(targetmesh,pos,w,h,d,placenoise,depthnoise,sizenoise){
 			// poisitional noise
           
-          if (dbg) Debug.log("prepos: "+pos.x + " " + pos.y + " " +pos.z);
+          if (dbg) Debug.log("prepos: "+pos);
           
-          if (dbg) Debug.log("pt2a: "+pt2a(pos));
-			var position_displacement = seededrandom3D(pos,1000.0,placenoise);
-			position_displacement.y = seededrandom1D(pos,1000.0,depthnoise);
-			pos = pos + position_displacement;
+          var position_displacement = seededrandom3Darr(pos,1000.0,placenoise);
+			position_displacement[1] = seededrandom1D(pos,1000.0,depthnoise);
+			pos = addmul(pos ,position_displacement, 1.0);
 			
 			//size noise (must always be lower than grout!)
 			var size_noise = seededrandom3D(pos,2000,sizenoise);
-			pos = pos - 0.5*size_noise  ;
-			w.x = w.x + 0.5*size_noise.x;
-			d.y = d.y + 0.5*size_noise.y;
-			h.z = h.z + 0.5*size_noise.z;
-          if (dbg) Debug.log("addbrick: "+pos.x + " " + pos.y + " " +pos.z);
+			
+			pos = addmul(pos ,size_noise ,-0.5) ;
+			w = w + 0.5*size_noise[0];
+			d = d + 0.5*size_noise[1];
+			h = h + 0.5*size_noise[2];
+          if (dbg) Debug.log("addbrick: "+pos);
 			//takes Point3D arguments as pos
 			//takes Vector3D as w,h,d
 			//var brick = new Mesh3D();
 			//targetmesh.quad(); // points
-            targetmesh.quad()// 12 
-          
-			targetmesh.quad(pt2a(pos), pt2a(pos + w), pt2a(pos + w + h), pt2a(pos+h)); //front face
-			targetmesh.quad(pt2a(pos + d), pt2a(pos + w + d), pt2a(pos + w + h +d), pt2a(pos + h + d)); //back face
-			targetmesh.quad(pt2a(pos), pt2a(pos + d), pt2a(pos + d + h), pt2a(pos+h)); //left face
-			targetmesh.quad(pt2a(pos + w), pt2a(pos + d + w), pt2a(pos + d + h +w) , pt2a(pos+h+w)); //right face
-			targetmesh.quad(pt2a(pos), pt2a(pos + d), pt2a(pos + d + w), pt2a(pos+w)); //bottom
-			targetmesh.quad(pt2a(pos + h),pt2a( pos + d + h), pt2a(pos + d + w + h), pt2a(pos+w + h)); //top
+            targetmesh.quad(pos[0],pos[1],pos[2],		pos[0] + w, pos[1],pos[2], 		pos[0] + w, pos[1], pos[2]+h,		pos[0],pos[1],pos[2]+h)// 12 
+            targetmesh.quad(pos[0],pos[1]+d,pos[2],		pos[0] + w, pos[1]+d,pos[2],	pos[0] + w, pos[1]+d, pos[2]+h,		pos[0],pos[1]+d,pos[2]+h)// 12 
+            targetmesh.quad(pos[0],pos[1],pos[2],		pos[0], pos[1]+d,pos[2],	pos[0], pos[1]+d, pos[2]+h,		pos[0],pos[1],pos[2]+h)// 12 
+            targetmesh.quad(pos[0]+w,pos[1],pos[2],		pos[0] + w, pos[1]+d,pos[2],	pos[0] + w, pos[1]+d, pos[2]+h,		pos[0]+w,pos[1],pos[2]+h)// 12 
+            targetmesh.quad(pos[0],pos[1],pos[2],		pos[0], pos[1]+d,pos[2],	pos[0] + w, pos[1]+d, pos[2],		pos[0]+w,pos[1],pos[2])// 12 
+            targetmesh.quad(pos[0],pos[1],pos[2]+h,		pos[0], pos[1]+d,pos[2]+h,	pos[0] + w, pos[1]+d, pos[2]+h,		pos[0]+w,pos[1],pos[2]+h)// 12 
+			
+			//targetmesh.quad(pt2a(pos), pt2a(pos + w), pt2a(pos + w + h), pt2a(pos+h)); //front face
+			//targetmesh.quad(pt2a(pos + d), pt2a(pos + w + d), pt2a(pos + w + h +d), pt2a(pos + h + d)); //back face
+			//targetmesh.quad(pt2a(pos), pt2a(pos + d), pt2a(pos + d + h), pt2a(pos+h)); //left face
+			//targetmesh.quad(pt2a(pos + w), pt2a(pos + d + w), pt2a(pos + d + h +w) , pt2a(pos+h+w)); //right face
+			//targetmesh.quad(pt2a(pos), pt2a(pos + d), pt2a(pos + d + w), pt2a(pos+w)); //bottom
+			//targetmesh.quad(pt2a(pos + h),pt2a( pos + d + h), pt2a(pos + d + w + h), pt2a(pos+w + h)); //top
 			return 0;
 		}
     var dbg = true;
@@ -104,9 +113,9 @@ var Vector3D = Core.Vector3D;
 	var placenoise = params ['placenoise'];
 	var sizenoise = params ['sizenoise'];
 	//var rotnoise = params ['rotnoise'];
-	var brickheight = Math.max(0,totalh/rows-grout);
+	var brickheight = Math.max(totalh/rows-grout);
     var brickwidth = 0;
-      var brickdepth = 0;
+    var brickdepth =  Math.max(0,totald,0);;
     var brickpos;
 	var brickwall = new Mesh3D();
 	//var brick
@@ -116,19 +125,15 @@ var Vector3D = Core.Vector3D;
 		if (r%2==1 && offset === true){
 			for (var c = 0; c < columns + 1; c++){
 				if (c === 0 || c == columns){ // small bricks
-					brickpos = new Point3D((totalw/columns)*(c +  0 ),0,(totalh/rows)*r);
-					brickpos.y = Math.sin(500*(brickpos.x+brickpos.z));
-					brickheight = new Point3D(0,0,Math.max(totalh/rows-grout));
-					brickwidth = new Point3D(Math.max((totalw/columns)*0.5-grout),0,0);
-					brickdepth = new Point3D(Math.max(0,depth,0));
+					brickpos = [(totalw/columns)*(c +  0 ),0,(totalh/rows)*r];
+					brickpos[1] = Math.sin(500*(brickpos[0]+brickpos[2]));
+					brickwidth = Math.max(0,(totalw/columns)*0.5-grout);
 					addbrick(brickwall,brickpos,brickwidth,brickheight,brickdepth,placenoise,depthnoise,sizenoise);
                     if (dbg) Debug.log("Smallbrick " +r + " "+c);
 				}else{ //regular bricks
-					brickpos = new Point3D((totalw/columns)*(c + 0.5),0,(totalh/rows)*r);
-					brickpos.y = Math.sin(500*(brickpos.x+brickpos.z));
-					brickheight = new Point3D(0,0,Math.max(totalh/rows-grout));
-					brickwidth = new Point3D(Math.max(totalw/columns-grout),0,0);
-					brickdepth = new Point3D(Math.max(0,depth,0));
+					brickpos = [(totalw/columns)*(c + 0.5),0,(totalh/rows)*r];
+					brickpos[1] = Math.sin(500*(brickpos[0]+brickpos[2]));
+					brickwidth = Math.max(0,totalw/columns-grout));
 					addbrick(brickwall,brickpos,brickwidth,brickheight,brickdepth,placenoise,depthnoise,sizenoise);
                     if (dbg) Debug.log("bigbrick " +r + " "+c);
 	
@@ -137,11 +142,9 @@ var Vector3D = Core.Vector3D;
 			
 		}else{ //regular bricks
 			for (var c = 0; c < columns; c++){
-				brickpos = new Point3D((totalw/columns)*c,0,(totalh/rows)*r);
-				brickpos.y = Math.sin(500*(brickpos.x+brickpos.z));
-				brickheight = new Point3D(0,0,Math.max(totalh/rows-grout));
-				brickwidth = new Point3D(Math.max(totalw/columns-grout),0,0);
-				brickdepth = new Point3D(Math.max(0,totald,0));
+				brickpos = [(totalw/columns)*c,0,(totalh/rows)*r];
+				brickpos[1] = Math.sin(500*(brickpos[0]+brickpos[2]));
+				brickwidth = Math.max(0,totalw/columns-grout));
 				addbrick(brickwall,brickpos,brickwidth,brickheight,brickdepth,placenoise,depthnoise,sizenoise);
                 if (dbg) Debug.log("bigbrick " +r + " "+c);
 			}
@@ -149,4 +152,4 @@ var Vector3D = Core.Vector3D;
 	  }
 	  //return addbrick(Point3D(0,0,0),Point3D(10,0,0),Point3D(0,10,0),Point3D(0,0,10),placenoise,depthnoise,sizenoise);
 	return Solid.make(brickwall);
-	}
+}
